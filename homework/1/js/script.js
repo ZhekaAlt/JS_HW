d5a0929a4cba36a537057a38ece3da705d8e4e27
ZhekaAlt <zhekaalt@gmail.com>
2014-10-08 19:29:58 +0300

    HW

---
+++ homework/1/js/script.js
@@ -0,0 +1,6 @@
+   
+    var fio=prompt("Введите ФИО:");
+    var pol=prompt("Введите пол:");
+    var age=prompt("Введите возраст:");
+    var email=prompt("Введите e-mail:");
+confirm("ФИО: "+fio+"\nПол: "+pol+"\nВозраст: "+age+"\ne-mail:"+e-mail+"\n\n\n"+"Все верно?");
